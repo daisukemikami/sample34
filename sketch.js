9278b3f83b7a0c9e38fbd651a88eb1f35a49a277
Daisuke Mikami <daisukemikami0325@gmail.com>
2019-07-22 13:08:08 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(600, 200);
+  background(0);
+  noStroke();
+  fill(255);
+
+  const step = 20;
+
+  for (let y = 0; y <= height; y += step) {
+    for (let x = 0; x <= width; x += step) {
+      const size = random(0, 15);
+      // fill(random(255));
+      // fill(random(255), random(255), random(255));
+      fill(random(200), random(255), random(100));
+      // const r = random(255);
+      // const g = random(r);
+      // const b = random(g);
+      // fill(r, g ,b);
+      ellipse(x, y, size, size);
+    }
+  }
+}
